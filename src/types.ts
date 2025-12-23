7a60f11c24b7caf4ea41c11231419924a5954b02
samugi <samuele@konghq.com>
2025-12-23 10:58:04 +0100

    small fix to types

--- src/types.ts
+++ src/types.ts
@@ -3,13 +3,6 @@ import type { Node } from 'reactflow'
 // edge kinds
 export type EdgeKind = 'flow' | 'data'
 
-// Call nodes data spec
-export type CallSpec = {
-    kind: string | 'call'
-    name: string
-    n_args: number
-}
-
 export type LiteralNodeData = {
     kind: 'literal'
     name: string
